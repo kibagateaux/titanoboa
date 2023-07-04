ec5fba56897c2b71d2559b4f1030a181e052325a
kibagateaux <kibagateaux@gmail.com>
2023-07-04 15:19:46 +0200

    simplify and clean up event tests

--- tests/unitary/strategy/test_event.py
+++ tests/unitary/strategy/test_event.py
@@ -1,80 +1,63 @@
-import itertools
-
-import pytest
-from hypothesis import given
-from hypothesis.strategies import SearchStrategy
-
 from boa.environment import Env
 from boa.interpret import load
-from boa.test import strategy
 from boa.vyper.event import Event
 
-BASE_TYPES = ["address", "bool", "bytes32", "decimal", "int", "string", "uint"]
+param_types = "(bool,uint8,address,bytes32)"
+event_type = {
+    "indexed": [False, True, True, False],
+    # order param keys so ordered_args() matches actual params
+    "arguments": {idx: value for idx, value in enumerate(param_types)},
+}
 
 
-@pytest.mark.parametrize("base1,base2", itertools.product(BASE_TYPES, BASE_TYPES))
-def test_strategy(base1, base2):
-    # ensure we can test on tuples
-    # tuple generates strategies for underlying types
-    assert isinstance(strategy(f"({base1},({base2},{base1}))"), SearchStrategy)
-    assert isinstance(strategy(f"({base1})"), SearchStrategy)
-    assert isinstance(strategy(f"({base2},{base2})"), SearchStrategy)
-    assert isinstance(strategy(f"(({base1},{base2}))"), SearchStrategy)
-
-
-@given(args=strategy("(bool,uint8,address,bytes32)"))
-def test_topics_saved_to_event(args):
-    event = Event("0x0", "0x0", "", args, [], {})
-    assert event.topics == args
+def test_topics_saved_to_event():
+    event_type["indexed"] = [True, True, True, True]
+    event = Event("0x0", "0x0", event_type, param_types, [])
+    assert event.topics == param_types
     assert event.args == []
     print(Event)
-    assert len(event.ordered_args()) == len(args)
+    assert len(event.ordered_args()) == len(param_types)
 
 
-@given(args=strategy("(bool,uint8,address,bytes32)"))
-def test_args_saved_to_event(args):
-    event = Event("0x0", "0x0", "", [], args, {})
-    assert event.args == args
+def test_args_saved_to_event():
+    event_type["indexed"] = [False, False, False, False]
+    event = Event("0x0", "0x0", event_type, [], param_types)
+    assert event.args == param_types
     assert event.topics == []
-    assert len(event.ordered_args()) == len(args)
+    assert len(event.ordered_args()) == len(param_types)
 
 
-@given(args=strategy("(bool,uint8,address,bytes32)"))
-def test_args_ordered_to_event_param_sequence(args):
+def test_args_ordered_to_event_param_sequence():
+    param_types = "(bool,uint8,address,bytes32)"
     # separate indexed from non-indexed in Event state
     # to make sure they get merged properly
-    topics = [args[1], args[2]]
-    params = [args[0], args[3]]
-    event_type = {
-        "indexed": [False, True, True, False],
-        # order param keys so ordered_args() matches actual params
-        "arguments": {idx: value for idx, value in enumerate(args)},
-    }
+    topics = [param_types[1], param_types[2]]
+    params = [param_types[0], param_types[3]]
 
     event = Event("0x0", "0x0", event_type, topics, params)
     assert event.args == params
     assert event.topics == topics
-    assert len(event.ordered_args()) == len(args)
+    assert len(event.ordered_args()) == len(param_types)
 
     for idx, key, value in enumerate(event.ordered_args()):
         assert key == event_type.values[idx]
-        assert value == args[idx]
+        assert value == param_types[idx]
 
 
-@given(args=strategy("(bool,uint8,address,bytes32)"))
-def test_args_map_values_match_ordered_args(args):
+def test_event_args_map_values_match_ordered_args():
+    param_types = "(bool,uint8,address,bytes32)"
     event_type = {
         "indexed": [False, True, True, False],
-        # order args so ordered_args() matches actual params
-        "arguments": {idx: value for idx, value in enumerate(args)},
+        # order_args so ordered_args() matches actual params
+        "arguments": {idx: value for idx, value in enumerate(param_types)},
     }
-    event = Event("0x0", "0x0", event_type, [], args)
+    event = Event("0x0", "0x0", event_type, [], param_types)
     for idx, k in enumerate(event_type["arguments"].keys()):
-        assert event.args_map[k] == args[idx]
+        assert event.args_map[k] == param_types[idx]
 
 
 def test_event_is_emitted():
-    me = Env.generate_address()
+    me = Env().generate_address()
     token = load("examples/ERC20.vy", "test", "TEST", 18, 0, sender=me)
     token.mint(me, 100, sender=me)
     mint_events = token.get_logs()
